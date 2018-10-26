9ffe08b8f99deaa040a71bcf8993b8e977923040
James Haller <jameshaller27@gmail.com>
2018-10-26 15:59:15 -0400

    Added help text via default card

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,78 @@
 const title = 'Cheat Sheeter';
 
+const exampleCard = '# Header 1\n' +
+    '## Header 2\n' +
+    '### Header 3\n' +
+    '#### Header 4\n' +
+    '##### Header 5\n' +
+    '###### Header 6\n' +
+    '\n' +
+    'Type bare words!\n' +
+    'New line...\n' +
+    '\n' +
+    'Double new line.\n' +
+    '\n' +
+    '* Lists work too\n' +
+    '* They have banded rows\n' +
+    '* This makes it easier to read\n' +
+    '* In fact, cards often look nice when they have only a list\n' +
+    '* Can we have sublists?\n' +
+    '    * Why yes.\n' +
+    '    * Yes we can\n' +
+    '    * with `* Text` (indent with spaces)\n' +
+    '\n' +
+    '---\n' +
+    '\n' +
+    'Dividers (`---`) are cool!\n' +
+    '\n' +
+    'We can also have tables...\n' +
+    '\n' +
+    '| Format | Symbols |\n' +
+    '|:------:|:-------:|\n' +
+    '| **bold** | `**bold**` |\n' +
+    '| _italics_ | `_italics_` |\n' +
+    '| ~~strikethrough~~ | ```~~strikethrough~~``` |\n' +
+    '| **_~~all of it~~_** | `**_~~all of it~~_**` |\n' +
+    '| `code` | ``` `code` ``` |\n' +
+    '\n' +
+    '_(strikethroughs are currently broken. GitHub issue posted)_\n' +
+    '\n' +
+    '---\n' +
+    '\n' +
+    'End page with `/end`\n' +
+    '\n' +
+    '/end\n' +
+    '\n' +
+    '* Will also reflow new cards (thanks to flex-boxes!)\n' +
+    '* &nbsp;\n' +
+    '* For now, color is permanent\n' +
+    '* But eventually I\'d like to implement custom color schemes\n' +
+    '* &nbsp;\n' +
+    '* Also, you can input a blank list item (like above) with `&nbsp;`\n' +
+    '\n' +
+    '/end\n' +
+    '\n' +
+    '# Save data\n' +
+    '* Currently login uses localStorage (saves to browser cache)\n' +
+    '    * So don\'t put anything sensitive here!\n' +
+    '    * And data can\'t be accessed from another browser/computer\n' +
+    '    * Plans are to use personal MongoDB Atlas cluster eventually\n' +
+    '## Features\n' +
+    '* Login with an existing account, or register for a new one\n' +
+    '* Only 1 save slot (save externally for multiple saves)\n' +
+    '\n' +
+    '/end\n' +
+    '\n' +
+    '# Future plans\n' +
+    '* Use actual database for users & save data\n' +
+    '* Add custom colors/theme\n' +
+    '* Add print stylesheet\n' +
+    '* Select number of columns (auto, 1, 2, ...)\n' +
+    '* Evenly-sized columns\n' +
+    '* Allow cards to span multiple columns\n' +
+    '* Multiple save slots\n' +
+    '* Multiple pages in single save';
+
 //=========================================================
 
 const app = angular.module('cheatsheetapp', ['ngSanitize','ng-showdown']);
@@ -67,7 +140,7 @@ app.directive('bindHtmlCompile', function($compile) {
 app.controller('mainCtrl', function ($scope) {
     $scope.title = title;
 
-    $scope.cardData = '# Example\n\nType with markdown here!';
+    $scope.cardData = exampleCard;
 
     $scope.proxy = LocalProxy;
 
